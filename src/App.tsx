9c3137ca090969a02c3392bed0adc2b6f9c8662a
Marceometry <marceometry@gmail.com>
2021-12-22 14:01:21 -0300

    refactor: separate score component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
+import { Score } from './components'
 import './styles/main.css'
 
 export function App() {
   return (
     <div>
-      <div className='score'>
-        <div id='player-score'>0</div>
-        <div id='computer-score'>0</div>
-      </div>
+      <Score />
+
       <div className='ball' id='ball'></div>
       <div className='paddle left' id='player-paddle'></div>
       <div className='paddle right' id='computer-paddle'></div>
---
+++ src/components/Score/index.tsx
@@ -0,0 +1,10 @@
+import './styles.css'
+
+export function Score() {
+  return (
+    <div className='score'>
+      <div id='player-score'>0</div>
+      <div id='computer-score'>0</div>
+    </div>
+  )
+}
