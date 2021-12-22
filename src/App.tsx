b791af4bc273602c41d8b37dd573788c2518772d
Marceometry <marceometry@gmail.com>
2021-12-22 13:40:21 -0300

    feat: add html

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-function App() {
-  return <div />
+export function App() {
+  return (
+    <div>
+      <div className='score'>
+        <div id='player-score'>0</div>
+        <div id='computer-score'>0</div>
+      </div>
+      <div className='ball' id='ball'></div>
+      <div className='paddle left' id='player-paddle'></div>
+      <div className='paddle right' id='computer-paddle'></div>
+    </div>
+  )
 }
-
-export default App
