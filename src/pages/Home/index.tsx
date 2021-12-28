039f59f2095328438d91ba4f9389b8c0b0a3fc37
Marceometry <marceometry@gmail.com>
2021-12-28 14:22:22 -0300

    feat: build home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,17 @@
 import { Link } from 'react-router-dom'
+import './styles.css'
 
 export function Home() {
   return (
-    <div>
-      <Link to='/play'>Jogar</Link>
+    <div className='home'>
+      <h1>Pong Clone</h1>
+
+      <div className='home-content'>
+        <Link to='/play'>Play</Link>
+        <Link to='/watch'>Watch mode</Link>
+      </div>
+
+      <span>Made by Marcelino Teixeira</span>
     </div>
   )
 }
