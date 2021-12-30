068f4a21c19690ef477409c1c5f834d74f9e74ca
Marceometry <marceometry@gmail.com>
2021-12-30 20:45:56 -0300

    style: improve settings form styles

--- src/pages/Home/components/SettingsModal/form/index.tsx
+++ src/pages/Home/components/SettingsModal/form/index.tsx
@@ -19,55 +19,57 @@ export function SettingsForm() {
 
   return (
     <div className='settings-form'>
-      <div>
-        <h2>Ball Velocity Increase</h2>
+      <Form.Wrapper handleSubmit={() => {}}>
+        <section>
+          <h2>Ball Velocity Increase</h2>
 
-        <Form.Wrapper handleSubmit={() => {}}>
-          <Form.Radio
-            label='Slow'
-            group='ballVelocityIncrease'
-            name={ballVelocityIncreaseModesKeys.SLOW}
-            handleCheck={handleCheck}
-            checked={
-              settings.ballVelocityIncrease ===
-              ballVelocityIncreaseModesKeys.SLOW
-            }
-          />
+          <div className='radio-container'>
+            <Form.Radio
+              label='Slow'
+              group='ballVelocityIncrease'
+              name={ballVelocityIncreaseModesKeys.SLOW}
+              handleCheck={handleCheck}
+              checked={
+                settings.ballVelocityIncrease ===
+                ballVelocityIncreaseModesKeys.SLOW
+              }
+            />
 
-          <Form.Radio
-            label='Medium'
-            group='ballVelocityIncrease'
-            name={ballVelocityIncreaseModesKeys.MEDIUM}
-            handleCheck={handleCheck}
-            checked={
-              settings.ballVelocityIncrease ===
-              ballVelocityIncreaseModesKeys.MEDIUM
-            }
-          />
+            <Form.Radio
+              label='Medium'
+              group='ballVelocityIncrease'
+              name={ballVelocityIncreaseModesKeys.MEDIUM}
+              handleCheck={handleCheck}
+              checked={
+                settings.ballVelocityIncrease ===
+                ballVelocityIncreaseModesKeys.MEDIUM
+              }
+            />
 
-          <Form.Radio
-            label='Fast'
-            group='ballVelocityIncrease'
-            name={ballVelocityIncreaseModesKeys.FAST}
-            handleCheck={handleCheck}
-            checked={
-              settings.ballVelocityIncrease ===
-              ballVelocityIncreaseModesKeys.FAST
-            }
-          />
+            <Form.Radio
+              label='Fast'
+              group='ballVelocityIncrease'
+              name={ballVelocityIncreaseModesKeys.FAST}
+              handleCheck={handleCheck}
+              checked={
+                settings.ballVelocityIncrease ===
+                ballVelocityIncreaseModesKeys.FAST
+              }
+            />
 
-          <Form.Radio
-            label='Insane'
-            group='ballVelocityIncrease'
-            name={ballVelocityIncreaseModesKeys.INSANE}
-            handleCheck={handleCheck}
-            checked={
-              settings.ballVelocityIncrease ===
-              ballVelocityIncreaseModesKeys.INSANE
-            }
-          />
-        </Form.Wrapper>
-      </div>
+            <Form.Radio
+              label='Insane'
+              group='ballVelocityIncrease'
+              name={ballVelocityIncreaseModesKeys.INSANE}
+              handleCheck={handleCheck}
+              checked={
+                settings.ballVelocityIncrease ===
+                ballVelocityIncreaseModesKeys.INSANE
+              }
+            />
+          </div>
+        </section>
+      </Form.Wrapper>
     </div>
   )
 }
